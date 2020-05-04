123fe6cb09ec0db697fb18c8531d0514c676a043
GeorgeCoops <coopsgeorge@gmail.com>
2020-05-04 18:02:01 +0100

    adding users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+      end
+
+      def show
+        
+       user = User.find_by_id(params[:id])
+        
+        render json: user
+    end
+
+    def new 
+        user = User.new
+    end
+
+    def create 
+        user = User.create(name: params[:name], points: 0, game_id: params[:game_id])
+    end
+
+
+
 end
